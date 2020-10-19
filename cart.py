ccea914fa49b8a9359950e156486e058013752fc
hillarys <hillarycamiloramirez@gmail.com>
2020-10-18 20:10:25 -0400

    cart.py la baraja esta creada

--- cart.py
+++ cart.py
@@ -4,35 +4,36 @@ diamantes = "\u2666"
 trebol = "\u2663"
 
 
-class baraja:
+class Cartas:
 
-    def __init__(self,tantos,palo):
-        self.total = total
-        self.palos = palo
+    def __init__(self,tanto,palo):
+        self.tanto = tanto
+        self.palo = palo
     
     def __str__(self):
-        return "[{}-{}]".format(self.tantos,self.palos)
+        return "[{}-{}]".format(self.tanto,self.palo)
 
-class Carta:
-    palos = ["\u2660","\u2665","\u2666", "\u2663"]
-    tantos = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-    self.mazo = []
-    for j in tantos:
-        for h in palos:
-        cartas = Carta(j,h)
-        self.mazo.append(cartas)
+class Baraja:
+    def __init__(self):
+        palos = ["\u2660","\u2665","\u2666", "\u2663"]
+        tantos = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        self.mazo = []
+        for j in tantos:
+            for h in palos:
+                carta = Cartas(j,h)
+                self.mazo.append(carta)
 
     def mostrar_baraja(self):
-        for num,Carta in enumerate(self.mazo):
-            if num % 4 != 0:
+        print()
+        for num,carta in enumerate(self.mazo):
+            if (num-3) % 4 != 0:
                 print(carta, end=" ")
             else:
-                print(Carta)
+                print(carta)
         print()
 
-baraja = baraja()
-baraja.mostrar_baraja()
-
+Baraja = Baraja()
+Baraja.mostrar_baraja()
 
 
 
